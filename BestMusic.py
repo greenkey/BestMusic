2c06d4e23156bcd743de1228764ff9140a28b3cb
Lorenzo Mele <github@loman.it>
2015-04-10 00:22:57 +0200

    first version, mocked

---
+++ BestMusic.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+"""
+BestMusic is a library to get music charts
+
+"""
+
+
+__author__ = "Lorenzo Mele"
+__credits__ = ["Lorenzo Mele"]
+__license__ = "GPL"
+__version__ = "0.1"
+__email__ = "lorenzo.mele@agavee.com"
+
+
+class BestMusic:
+    
+    def getChart(self, year=None):
+        return [
+            {
+                'artist': 'Dolphins',
+                'title': 'So Long, and Thanks for All the Fish'
+            },{
+                'artist': 'Led Zeppelin',
+                'title': 'Stairway to Heaven'
+            },{
+                'artist': 'Dire Straits',
+                'title': 'Romeo and Juliet'
+            }
+        ]
+
+
+
+
+###############################################################################
+# main
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    from util.util import Logger
+
+    ###########################################################################
+    # parse parameters and init
+
+    mylog = Logger()
+    log = mylog.log
+
+    parser = OptionParser()
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      help="print useful output for debugging")
+    parser.add_option("--year",
+                      dest="year",
+                      help="set the year of analysis")
+
+
+    (options, args) = parser.parse_args()
+
+    mylog.setVerbose(options.verbose)
+
+    log("options:")
+    log(options)
+    log("args:")
+    log(args)
+
+    ###########################################################################
+    # 
+
+    bm = BestMusic()
+
+    if args[0] == "chart":
+
+        chart = bm.getChart(year=options.year)
+
+        print("Chart for year {}".format(options.year))
+        for i in range(len(chart)):
+            print("{}. {} - {}".format(
+                i+1,
+                chart[i]['artist'],
+                chart[i]['title']
+            ))
