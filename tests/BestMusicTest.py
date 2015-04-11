e10bf37b206e326a3a46c5367c01724490c12e8f
Lorenzo Mele <github@loman.it>
2015-04-11 19:31:07 +0200

    changed test accordingly to refactoring

--- tests/BestMusicTest.py
+++ tests/BestMusicTest.py
@@ -7,7 +7,7 @@ import os
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-from BestMusic import BestMusic
+from bestmusic import BestMusic
 
 
 class TestBestMusic(unittest.TestCase):
