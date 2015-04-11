60843c129e4227f3c3d36937404e3dced62423ab
Lorenzo Mele <github@loman.it>
2015-04-11 19:30:21 +0200

    removed class BestMusic to another file than the one executable

--- run.py
+++ run.py
@@ -12,20 +12,8 @@ __license__ = "GPL"
 __version__ = "0.1"
 __email__ = "lorenzo.mele@agavee.com"
 
-class BestMusic:
-    providers = list()
 
-    def __init__(self):
-        import pkgutil
-        import providers
-        from importlib import import_module
-        for importer, modname, ispkg in pkgutil.iter_modules(providers.__path__):
-            self.providers.append( import_module('providers.' + modname).Provider() )
-     
-    def getChart(self, year=None):
-        return self.providers[0].getChart(year=year)
-
-
+from bestmusic import BestMusic
 
 
 ###############################################################################
